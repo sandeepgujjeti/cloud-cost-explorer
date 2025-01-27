6896cfea9f418755c393cefc6670b2d5e8e8e44a
Charan Y <racha.coder@gmail.com>
2025-01-27 10:24:56 +0530

    correct the styling in the SideBar Component

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,20 +5,19 @@ const SideBar = () => {
   return (
     <div>
       <div class="sidebar">
-      
+
         <h1>Cloud Cost explorer</h1>
         <ul>
-            <li><a href="#">Team Wise Analysis</a></li>
-            <li><a href="#">Date Wise Analysis</a></li>
-            <li><a href="#">Service Wise Analysis</a></li>
-            
+          <li><a href="#">Team Wise Analysis</a></li>
+          <li><a href="#">Date Wise Analysis</a></li>
+          <li><a href="#">Service Wise Analysis</a></li>
         </ul>
-    </div>
-    <div class="content">
+      </div>
+      {/* <div class="content">
         <h1>Welcome to the Sidebar Page</h1>
         <p>This is a simple page layout with a sidebar navigation menu. Use the links in the sidebar to navigate to different sections of the site.</p>
-    </div>
-      
+    </div> */}
+
     </div>
   )
 }
