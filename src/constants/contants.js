2629fcd5220118b9d41d847ea82da226fadbe876
Charan Y <racha.coder@gmail.com>
2025-02-04 07:43:31 +0530

    refactor files into their own folders

---
+++ src/constants/contants.js
@@ -0,0 +1,9 @@
+const Team = "Team Wise Analysis";
+const Date = "Date Wise Analysis";
+const Service = "Service Wise Analysis";
+
+export const analysisTypes = {
+    team: Team,
+    date: Date,
+    service: Service
+};
