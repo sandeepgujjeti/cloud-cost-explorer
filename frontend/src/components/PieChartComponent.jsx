3bbde11cbb7f2265a6750a160a81d6c92027b4e2
tejasreeshaga28 <tejasreeshaga28@gmailcom>
2025-03-03 10:59:25 +0530

    pie chart component,fix side bar

--- frontend/src/components/PieChartComponent.jsx
+++ frontend/src/components/PieChartComponent.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
+import {
+  PieChart,
+  Pie,
+  Cell,
+  Label,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+// import { pieData, COLORS } from '../constants/constants.js';
+// import { analysisTypes } from 'constants/constants.js'
 
 const PieChartComponent = () => {
-  return (
-    <div>PieChartComponent</div>
-  )
-}
+  const pieData = [
+    { name: "Product A", value: 9.9 },
+    { name: "Product B", value: 13.2 },
+    { name: "Product C", value: 10.2 },
+    { name: "Product D", value: 12.6 },
+    { name: "Product E", value: 8.1 },
+    { name: "Product F", value: 7.3 },
+    { name: "Product G", value: 7.9 },
+    { name: "Product H", value: 10.9 },
+    { name: "Product I", value: 10.8 },
+    { name: "Product J", value: 9.0 },
+  ];
 
-export default PieChartComponent
+  const COLORS = [
+    "#1b5e2",
+    "#2e7d32",
+    "#388e3c",
+    "#43a047",
+    "#4caf50",
+    "#66bb6a",
+    "#81c784",
+    "#a5d6a7",
+    "#c8e6c9",
+    "#e8f5e9",
+  ];
+  // <div>PieChartComponent
+  return (
+    <ResponsiveContainer width={400} height={400}>
+          <PieChart width={"100%"} height={"100%"}>
+        <Pie
+          data={pieData}
+          dataKey="value"
+          cx="50%"
+          cy="50%"
+          innerRadius={50}
+          fill="#8884d8"
+          paddingAngle={5}
+          label
+        >
+          {pieData.map((entry, index) => (
+            <Cell key={index} fill={COLORS[index]} />
+          ))}
+        </Pie>
+        <Label />
+        <Legend layout="vertical" verticalAlign="middle" align="right" />
+      </PieChart>
+    </ResponsiveContainer>
+  );
+  // </div>
+};
+
+export default PieChartComponent;
--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -1,17 +1,14 @@
-const Team = "Team Wise Analysis";
-const Date = "Date Wise Analysis";
-const Service = "Service Wise Analysis";
+const Overall = "overall";
+const Team = "team";
+const Product = "product";
 
 export const analysisTypes = {
+    overall: Overall,
     team: Team,
-    date: Date,
-    service: Service
+  Product: Product,
 };
 
-
-export const teams = [
-    "Finance", "DevOps", "Security", "R&D", "Analytics"
-];
+export const teams = ["Finance", "DevOps", "Security", "R&D", "Analytics"];
 // export const teams = [
 //     "team_001",
 //     "team_002",
@@ -20,37 +17,78 @@ export const teams = [
 //     "team_005"
 // ];
 
-
 export const dates = [
-    "yesterday", "last_week", "last_month", "quarter_year", "half_year", "last_year"
+  "yesterday",
+  "last_week",
+  "last_month",
+  "quarter_year",
+  "half_year",
+  "last_year",
 ];
 
-export const services = [
-    "AWS", "Azure", "Google_cloud", "IBM"
-];
-
+export const services = ["AWS", "Azure", "Google_cloud", "IBM"];
 
 export const xAxisValues = [
-
-    "servicename",
-    "sourcecloud",
-    "billedcapacity",
-    "billedamount",
-    "usedcapacity",
-    "serviceusedpercentage",
-    "utilizedamount",
-    "teamid",
-    "monitoredstarttime",
-    "monitoredendtime",
-    "interval_mins",
+  "servicename",
+  "sourcecloud",
+  "billedcapacity",
+  "billedamount",
+  "usedcapacity",
+  "serviceusedpercentage",
+  "utilizedamount",
+  "teamid",
+  "monitoredstarttime",
+  "monitoredendtime",
+  "interval_mins",
 ];
 
 export const yAxisValues = [
-    "billedcapacity",
-    "billedamount",
-    "usedcapacity",
-    "serviceusedpercentage",
-    "utilizedamount",
-    "interval_mins",
-] 
+  "billedcapacity",
+  "billedamount",
+  "usedcapacity",
+  "serviceusedpercentage",
+  "utilizedamount",
+  "interval_mins",
+];
 
+export const pieData = [
+  { name: "Product A", value: 9.9 },
+  { name: "Product B", value: 13.2 },
+  { name: "Product C", value: 10.2 },
+  { name: "Product D", value: 12.6 },
+  { name: "Product E", value: 8.1 },
+  { name: "Product F", value: 7.3 },
+  { name: "Product G", value: 7.9 },
+  { name: "Product H", value: 10.9 },
+  { name: "Product I", value: 10.8 },
+  { name: "Product J", value: 9.0 },
+];
+
+export const COLORS = [
+  "#1b5e2",
+  "#2e7d32",
+  "#388e3c",
+  "#43a047",
+  "#4caf50",
+  "#66bb6a",
+  "#81c784",
+  "#a5d6a7",
+  "#c8e6c9",
+  "#e8f5e9",
+];
+
+export const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index,
+}) => {
+  const RADIAN = Math.PI / 180;
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.65;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  0;
+};
