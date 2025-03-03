0d2a6ef5ed671f13cc7214419978c9ebcc3f75b6
sandeepgujjeti <bittusandeep074@gmail.com>
2025-03-03 11:09:09 +0530

    complete linechart,expenditurecard

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -52,5 +52,36 @@ export const yAxisValues = [
     "serviceusedpercentage",
     "utilizedamount",
     "interval_mins",
-] 
+];
+export const pieData = [
+    { name: "Product A", value: 9.9 },
+    { name: "Product B", value: 13.2 },
+    { name: "Product C", value: 10.2 },
+    { name: "Product D", value: 12.6 },
+    { name: "Product E", value: 8.1 },
+    { name: "Product F", value: 7.3 },
+    { name: "Product G", value: 7.9 },
+    { name: "Product H", value: 10.9 },
+    { name: "Product I", value: 10.8 },
+    { name: "Product J", value: 9.0 },
+];
+
+export const COLORS = ["#1b5e2", "#2e7d32", "#388e3c", "#43a047", "#4caf50",
+    "#66bb6a", "#81c784", "#a5d6a7", "#c8e6c9", "#e8f5e9"];
+
+export const lineChartdata = [
+    { month: "Jan", Alpha: 900, Beta: 800, Gamma: 400, Delta: 500, Meu: 300 },
+    { month: "Feb", Alpha: 850, Beta: 750, Gamma: 450, Delta: 550, Meu: 350 },
+    { month: "Mar", Alpha: 800, Beta: 700, Gamma: 500, Delta: 600, Meu: 400 },
+    { month: "Apr", Alpha: 750, Beta: 650, Gamma: 550, Delta: 650, Meu: 450 },
+    { month: "May", Alpha: 700, Beta: 600, Gamma: 600, Delta: 700, Meu: 500 },
+    { month: "Jun", Alpha: 650, Beta: 550, Gamma: 650, Delta: 750, Meu: 550 },
+    { month: "Jul", Alpha: 600, Beta: 500, Gamma: 700, Delta: 800, Meu: 600 },
+    { month: "Aug", Alpha: 550, Beta: 450, Gamma: 750, Delta: 850, Meu: 650 },
+    { month: "Sep", Alpha: 500, Beta: 400, Gamma: 800, Delta: 900, Meu: 700 },
+    { month: "Oct", Alpha: 450, Beta: 350, Gamma: 850, Delta: 950, Meu: 750 },
+    { month: "Nov", Alpha: 400, Beta: 300, Gamma: 900, Delta: 1000, Meu: 800 },
+    { month: "Dec", Alpha: 350, Beta: 250, Gamma: 950, Delta: 1050, Meu: 850 },
+];
+
 
