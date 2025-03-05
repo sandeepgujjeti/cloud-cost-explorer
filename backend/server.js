5c16d8555bf1f5206cd6a7656bc1ddab49756c61
Charan Y <racha.coder@gmail.com>
2025-03-05 10:11:06 +0530

    add backend code for db connection to supabase

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,33 @@
-const express = require('express');
+import express, { json } from 'express';
+import "dotenv/config";
+import client from './connection.js';
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
 // Middleware
-app.use(express.json());
+app.use(json());
+app.use(cors());
 
 // Routes
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/', async (req, res) => {
+    // res.send('Hello World!');
+    const query = `
+        SELECT 
+            TRIM(TO_CHAR("MonitoredStartTime", 'Month')) AS month_name,
+            COUNT(*) AS total_sales,  
+            SUM("UtilizedAmount") AS total_revenue,
+            EXTRACT(MONTH FROM "MonitoredStartTime") AS month_number
+        FROM cloud_costs
+        WHERE 
+            "MonitoredStartTime" >= DATE_TRUNC('month', CURRENT_DATE) - INTERVAL '12 months'
+            AND "ServiceName" = 'RDS'
+        GROUP BY month_name, month_number
+        ORDER BY month_number;
+    `;
+    const data = client.query(query);
+    res.send((await data).rows);
 });
 
 // Start server
