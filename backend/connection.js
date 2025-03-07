1f5cad04facc81c16f124c8c9540b522b9c33648
Charan Y <racha.coder@gmail.com>
2025-03-07 13:52:47 +0530

    add APIs for overall and team for PieChartComponent

--- backend/connection.js
+++ backend/connection.js
@@ -1,12 +1,5 @@
-// import { createClient } from "@supabase/supabase-js";
-
-// const supabaseURL = 'https://ocmofybqmclyaqiftkha.supabase.co';
-// const supabaseAPI_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jbW9meWJxbWNseWFxaWZ0a2hhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAzMTU5NjQsImV4cCI6MjA1NTg5MTk2NH0.kb101UUEwV341fnAbWF59Xutl-wGyA2xXQGmu_JsRnU';
-// const supabase = createClient(supabaseURL, supabaseAPI_KEY);
-
-// export default supabase;
-
 import pkg from "pg";
+
 const { Pool } = pkg;
 
 const pool = new Pool({
--- backend/server.js
+++ backend/server.js
@@ -11,20 +11,36 @@ app.use(json());
 app.use(cors());
 
 // Routes
-app.get('/', async (req, res) => {
+app.get('/overall', async (req, res) => {
     // res.send('Hello World!');
     const query = `
         SELECT 
             TRIM(TO_CHAR("MonitoredStartTime", 'Month')) AS month_name,
             COUNT(*) AS total_sales,  
-            SUM("UtilizedAmount") AS total_revenue,
+            SUM("UtilizedAmount") AS total_cost,
             EXTRACT(MONTH FROM "MonitoredStartTime") AS month_number
         FROM cloud_costs
         WHERE 
             "MonitoredStartTime" >= DATE_TRUNC('month', CURRENT_DATE) - INTERVAL '12 months'
-            AND "ServiceName" = 'RDS'
         GROUP BY month_name, month_number
         ORDER BY month_number;
+    `;
+    const data = client.query(query);
+    res.send((await data).rows);
+});
+
+app.get('/team', async (req, res) => {
+    // res.send('Hello World!');
+    const query = `
+        select distinct
+            "TeamId",
+            SUM("UtilizedAmount") as total_cost
+        from
+            cloud_costs
+        group by 
+            "TeamId"
+        order by
+            "TeamId";
     `;
     const data = client.query(query);
     res.send((await data).rows);
