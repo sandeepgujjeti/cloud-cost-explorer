2f42b32edd306d2f293a21861719a6299cd0ae9c
Charan Y <racha.coder@gmail.com>
2025-03-20 17:37:22 +0530

    configure .env for firebase

--- frontend/src/auth/firebaseConfig.js
+++ frontend/src/auth/firebaseConfig.js
@@ -2,17 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCgqx4zouyAAigYQWLf6aGh4IKoduGdJ7Q",
-  authDomain: "cloud-cost-explorer-dc3c7.firebaseapp.com",
-  projectId: "cloud-cost-explorer-dc3c7",
-  storageBucket: "cloud-cost-explorer-dc3c7.firebasestorage.app",
-  messagingSenderId: "67367082428",
-  appId: "1:67367082428:web:1a48536c8b8146858bb3b9"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-
 
 export { app, auth };
