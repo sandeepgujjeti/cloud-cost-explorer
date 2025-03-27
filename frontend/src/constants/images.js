81fc6ba12e00a1160542e24dd613d9adc28840c2
Charan Y <racha.coder@gmail.com>
2025-03-27 16:24:58 +0530

    add landing page images

---
+++ frontend/src/constants/images.js
@@ -0,0 +1,17 @@
+import actions from "../assets/actions.png";
+import barChart1 from "../assets/bar-1.png";
+import barLine1 from "../assets/bar-line-1.png";
+import barLine2 from "../assets/bar-line-2.png";
+import graphNotes from "../assets/graph-notes.png";
+import cloud from "../assets/cloud/png";
+
+const images = {
+    actions,
+    barChart1,
+    barLine1,
+    barLine2,
+    cloud,
+    graphNotes
+}
+
+export default images; 
